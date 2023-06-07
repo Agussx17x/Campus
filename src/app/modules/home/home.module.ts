96e4133606efd35704249c8543c89c852f41e10c
Aguss <agussx17x@gmail.com>
2023-06-07 11:29:24 -0300

    se creo el modulo y el componente home, en la carpeta pages

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
