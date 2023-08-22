0dc28e928f70c3aeaee023704365ebd6f7105517
Cachurmino <nachocappellini@gmail.com>
2023-08-22 08:21:40 -0300

    Instalación y configuración del login basico

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   hide = true;
+
+  //Cambiar Active para modo Responsive
+  //Agregamos la clase Active
+  activeSignup() {
+    let formBx = document.getElementById('formBx');
+    let body = document.getElementById('body');
+
+    formBx?.classList.add('active');
+    body?.classList.add('active');
+  }
+  //Removemos la clase Active
+  activeSignin() {
+    let formBx = document.getElementById('formBx');
+    let body = document.getElementById('body');
+
+    formBx?.classList.remove('active');
+    body?.classList.remove('active');
+  }
 }
