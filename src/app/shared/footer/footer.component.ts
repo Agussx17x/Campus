932ff580e431e7d938a1a30fec7284f060821e17
Aguss <agussx17x@gmail.com>
2023-08-23 15:29:04 -0300

    cambios de el footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  constructor(private router:Router){}
+
+  shouldShowNavbar(): boolean {
+    // Obtener la URL actual
+    const currentUrl = this.router.url;
+    
+    // Verificar si la URL es la de la página de inicio de sesión
+    return currentUrl !== '/login'; // Ajusta la URL según tu enrutamiento
+  }
 
 }
