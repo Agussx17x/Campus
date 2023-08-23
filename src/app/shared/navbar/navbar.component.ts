242baa149767bd9a94870e683389f3db66ebef9d
Aguss <agussx17x@gmail.com>
2023-08-23 15:15:39 -0300

    que no se muestre la navbar en el login

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +8,21 @@ import { Component } from '@angular/core';
 })
 
 export class NavbarComponent {
+
+  constructor(private router: Router){}
+
   //Agregamos y quitamos la clase active par ala responsividad.
   claseActive() {
     let header = document.querySelector('header');
     header?.classList.toggle('active');
   }
   icon : string = 'person_circle'
+  
+  shouldShowNavbar(): boolean {
+    // Obtener la URL actual
+    const currentUrl = this.router.url;
+    
+    // Verificar si la URL es la de la página de inicio de sesión
+    return currentUrl !== '/login'; // Ajusta la URL según tu enrutamiento
+  }
 }
