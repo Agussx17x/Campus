1cd644a6e0e348c9bedc4c8e6aa55f25042ce653
Aguss <agussx17x@gmail.com>
2023-09-05 09:33:56 -0300

    agregado de storage al proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,14 @@
 // ARCHIVO DE DESARROLLO
 export const environment = {
+  firebase: {
+    projectId: 'campusv2-72dd0',
+    appId: '1:1064512680520:web:0c55da0b2e81045ade5e7d',
+    storageBucket: 'campusv2-72dd0.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD35trHGp2nZYM7de0iVx6MLPAXrBy5Seg',
+    authDomain: 'campusv2-72dd0.firebaseapp.com',
+    messagingSenderId: '1064512680520',
+  },
     production: false,
     // SACAMOS CONST, EL "=" LO CAMBIAMOS POR ":" Y SACAMOS EL ";" FINAL
     firebaseConfig : {
