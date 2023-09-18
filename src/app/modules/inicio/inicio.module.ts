69d33be30cb128d7dbac9c585787d8c9ff8f4cde
FrankzeB <franezequielbravo@outlook.com>
2023-09-18 09:28:11 -0300

    docente-materias terminado

--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { InicioRoutingModule } from './inicio-routing.module';
 import { InicioComponent } from './page/inicio/inicio.component';
-import { MateriasComponent } from './home/components/materias/materias.component';
+
 
 @NgModule({
-    declarations: [
-        InicioComponent,
-        MateriasComponent,
-    ],
-    imports: [
-        InicioRoutingModule,
-    ],
-    exports: [
-        InicioComponent,
-    ],
+  declarations: [InicioComponent],
+  imports: [InicioRoutingModule],
+  exports: [InicioComponent],
 })
-export class InicioModule { }
+export class InicioModule {}
