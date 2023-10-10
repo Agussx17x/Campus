288c16569236d084d93db3bff73e2d105470b6fc
Aguss <agussx17x@gmail.com>
2023-10-10 17:44:47 -0300

    coreccion de errores en la main

--- src/app/modules/inicio/page/home/home.module.ts
+++ src/app/modules/inicio/page/home/home.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
-
 //Componentes
 import { HomeAdminComponent } from './home-admin/home-admin.component';
 import { HomeEstudianteComponent } from './home-estudiante/home-estudiante.component';
@@ -13,16 +11,16 @@ import { CardComponent } from './components/card-materias/card.component';
 import { CalendaryComponent } from './components/calendary/calendary.component';
 import { FormMatComponent } from './components/form-mat/form-mat.component';
 import { MateriaComponent } from './components/materia/materia.component';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 //Modulos
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
-import { PerfilComponent } from './components/perfil/perfil.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSlideToggleModule,_MatSlideToggleRequiredValidatorModule,} from '@angular/material/slide-toggle';
-import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule,_MatSlideToggleRequiredValidatorModule } from '@angular/material/slide-toggle';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +32,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CalendaryComponent,
     FormMatComponent,
     MateriaComponent,
-    PerfilComponent
+    PerfilComponent,
     ListaUsuariosComponent,
   ],
   imports: [
