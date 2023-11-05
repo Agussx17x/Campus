c5adbca3348898fbc2911c08595f73ede3f6fec7
Aguss <agussx17x@gmail.com>
2023-11-04 21:08:20 -0300

    creacion de secciones y material para el alumno

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,168 +1,65 @@
 import { Component } from '@angular/core';
-import { Trabajos } from 'src/app/models/trabajos';
 import { CrudService } from '../../services/crud.service';
-import { SeccionesService } from '../../services/secciones.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import { Seccion } from 'src/app/models/seccion';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
+
+
 export class TableComponent {
-  constructor(
-    public servicioCrud: CrudService,
-    private seccionesService: SeccionesService
-  ) {}
+  constructor(private crudService: CrudService) {}
+  secciones: Seccion[] = [];
 
-  coleccionTrabajos: Trabajos[] = [];
+  nuevaSeccion = { titulo: '', descripcion: '' };
+  
+  nuevoMaterial = {
+    titulo: '',
+    descripcion: '',
+    fechaEntrega: '',
+    urlDescarga: '',
+  };
 
-  trabajoSeleccionado!: Trabajos; // Toma valores vacios
+  materialesPorSeccion: any = {};
 
-  modalVisibleTrabajo: boolean = true;
 
-  secciones: any[] = [];
+  ngOnInit() {
+    this.obtenerSecciones();
+  }
 
-  //  modalVisible: boolean = false;
-
-  //  eliminarVisible: boolean = false;
-
-  // FormGroup de trabajos
-  trabajo = new FormGroup({
-    //Docs
-    titulo: new FormControl('', Validators.required),
-    docs: new FormControl('', Validators.required),
-    descripcion: new FormControl('', Validators.required),
-
-    //Imagen
-    /*
-    imagen: new FormControl('', Validators.required),
-    comentario: new FormControl('', Validators.required),
-    alt: new FormControl('', Validators.required),
-    */
-  });
-
-  // FormGroup de secciones
-  seccion = new FormGroup({
-    titulo: new FormControl('', Validators.required),
-    descripcion: new FormControl(''),
-  });
-
-  ngOnInit(): void {
-    // Cargar los trabajos desde Firebase
-    this.servicioCrud.obtenerTrabajos().subscribe((trabajos) => {
-      this.coleccionTrabajos = trabajos;
-    });
-
-    // Cargar las secciones desde Firebase
-    this.seccionesService.obtenerSecciones().subscribe((secciones) => {
+  obtenerSecciones() {
+    this.crudService.obtenerSecciones().subscribe((secciones) => {
       this.secciones = secciones;
+      for (let seccion of secciones) {
+        this.obtenerMateriales(seccion.idSeccion);
+      }
     });
   }
 
-  // Esta función añade un nuevo trabajo.
-  async agregarTrabajo() {
-    if (this.trabajo.valid) {
-      let nuevoTrabajo: Trabajos = {
-        idTrabajo: 0,
-        fecha:'',
-        titulo: this.trabajo.value.titulo!,
-        descripcion: this.trabajo.value.descripcion!,
-        docs: this.trabajo.value.docs!,
+  crearSeccion() {
+    this.crudService.crearSeccion(this.nuevaSeccion).then(() => {
+      this.nuevaSeccion = { titulo: '', descripcion: '' };
+      this.obtenerSecciones();
+    });
+  }
+
+  obtenerMateriales(idSeccion:any) {
+    this.crudService.obtenerMateriales(idSeccion).subscribe(materiales => {
+      this.materialesPorSeccion[idSeccion] = materiales;
+    });
+  }
+
+  crearMaterial(idSeccion:any) {
+    this.crudService.crearMaterial(idSeccion, this.nuevoMaterial).then(() => {
+      this.nuevoMaterial = {
+        titulo: '',
+        descripcion: '',
+        fechaEntrega: '',
+        urlDescarga: '',
       };
-
-      await this.servicioCrud
-        .crearTrabajos(nuevoTrabajo)
-        .then((trabajos) => {
-          alert('ha agregado un nuevo trabajo con exito');
-          
-        })
-        .catch((error) => {
-          alert('Hubo un error al cargar un nuevo trabajo \n' + error);
-        });
-      this.trabajo.reset();
-    }
-  }
-
-  // Esta función muestra los valores en los inputs.
-  mostrarEdit(trabajoSeleccionado: Trabajos) {
-    this.trabajoSeleccionado = trabajoSeleccionado;
-    this.trabajo.setValue({
-      titulo: trabajoSeleccionado.titulo,
-      descripcion: trabajoSeleccionado.descripcion,
-      docs: trabajoSeleccionado.docs,
+      this.obtenerMateriales(idSeccion);
     });
-  }
-
-  // Esta función edita o actualiza un trabajo existente.
-  editTrabajo() {
-    let datos: Trabajos = {
-      idTrabajo: 0,
-      fecha:'',
-      titulo: this.trabajo.value.titulo!,
-      descripcion: this.trabajo.value.descripcion!,
-      docs: this.trabajo.value.docs!,
-    };
-
-    this.trabajo.reset();
-
-    this.servicioCrud
-      .editTrabajo(this.trabajoSeleccionado.idTrabajo, datos)
-      .then((trabajos) => {
-        alert('Se editó correctamente');
-      })
-      .catch((error) => {
-        alert('No se pudo modificar \n' + error);
-      });
-  }
-
-  // Esta función muestra el delete en el modal.
-  mostrarDelete(trabajoSeleccionado: Trabajos) {
-    this.trabajoSeleccionado = trabajoSeleccionado;
-  }
-
-  // Esta función elimina un trabajo creado.
-  deleteTrabajo() {
-    this.servicioCrud
-      .deleteTrabajo(this.trabajoSeleccionado.idTrabajo)
-      .then((respuesta) => {
-        alert('borrado con exito');
-      })
-      .catch((error) => {
-        alert('No se elimino el trabajo \n' + error);
-      });
-  }
-
-  // Esta función sube un archivo o imagenes a la base.
-  uploadFile(event: any) {
-    const file = event.target.files[0];
-    const storage = getStorage();
-    const storageRef = ref(
-      storage,
-      'docs/' + this.trabajoSeleccionado.idTrabajo
-    ); //Como nombre del documento, le pongo el id con el que se guardan los datos
-    uploadBytes(storageRef, file).then((snapshot) => {
-      console.log('Archivo cargado con exito!!');
-    });
-  }
-
-  // Esta función agrega una nueva sección.
-  agregarSeccion() {
-    // Comprueba si el formulario de sección es válido.
-    if (this.seccion.valid) {
-      // Obtiene los valores del formulario y los almacena en un objeto nuevaSeccion.
-      const nuevaSeccion = {
-        titulo: this.seccion.get('titulo')?.value,
-        descripcion: this.seccion.get('descripcion')?.value,
-      };
-
-      // Agregar la nueva sección a Firebase
-      this.seccionesService.agregarSeccion(nuevaSeccion).then((resultado) => {
-        // Muestra un mensaje de éxito después de agregar la sección.
-        alert('Sección agregada con éxito');
-        this.seccion.reset(); // Limpia el formulario después de agregar la sección.
-      });
-    }
   }
 }
--- src/app/modules/inicio/page/home/home-admin/home-admin.component.ts
+++ src/app/modules/inicio/page/home/home-admin/home-admin.component.ts
@@ -24,39 +24,7 @@ export class HomeAdminComponent {
   constructor(public servicioCrud: CrudService, private router: Router) {}
 
   ngOnInit(): void {
-    this.servicioCrud.obtenerTrabajos().subscribe((trabajos) => {
-      this.coleccionTrabajos = trabajos;
-    });
-  }
-
-  async agregarTrabajo(event: any) {
-    if (this.trabajo.valid) {
-      let nuevoTrabajo: Trabajos = {
-        idTrabajo: 0,
-        fecha: this.trabajo.value.fecha!,
-        titulo: this.trabajo.value.titulo!,
-        descripcion: this.trabajo.value.descripcion!,
-        docs: this.trabajo.value.docs!,
-      };
-      const file = event.target.files[0];
-      const storage = getStorage();
-      const storageRef = ref(
-        storage,
-        'docs/' + '/' + this.trabajo.value.fecha + this.trabajo.value.titulo
-      );
-      uploadBytes(storageRef, file).then((snapshot) => {
-        console.log('Archivo cargado con exito!!');
-      });
-
-      await this.servicioCrud
-        .crearTrabajos(nuevoTrabajo)
-        .then((trabajos) => {
-          alert('ha agregado un nuevo trabajo con exito');
-        })
-        .catch((error) => {
-          alert('Hubo un error al cargar un nuevo trabajo \n' + error);
-        });
-    }
+    
   }
 
   selectDay!: string;
