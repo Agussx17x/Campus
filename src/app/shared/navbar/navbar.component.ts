f64384f5edf1099b168685286ebbf49493704430
FrankzeB <franezequielbravo@outlook.com>
2023-11-16 22:22:09 -0300

    Mejoras en el navbar y optimización de rutas seguras

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -8,15 +8,17 @@ import { AuthService } from 'src/app/modules/auth/services/auth.service';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  icon: string = 'person_circle';
+
   constructor(private router: Router, private afAuth: AuthService) {}
 
-  //Agregamos y quitamos la clase active par ala responsividad.
+  // Esta función agrega y quita la clase 'active' para la responsividad.
   claseActive() {
     let header = document.querySelector('header');
     header?.classList.toggle('active');
   }
-  icon: string = 'person_circle';
 
+  // Esta función verifica si la URL actual es diferente de '/login'. Si es así, muestra la barra de navegación.
   shouldShowNavbar(): boolean {
     // Obtener la URL actual
     const currentUrl = this.router.url;
@@ -25,23 +27,64 @@ export class NavbarComponent {
     return currentUrl !== '/login'; // Ajusta la URL según tu enrutamiento
   }
 
+  // Esta función verifica si la URL actual está en la lista de rutas donde el botón de inicio no debe mostrarse. Si es así, no muestra el botón de inicio.
+  showInicio(): boolean {
+    // Obtener la URL actual
+    const currentUrl = this.router.url;
+
+    // Lista de rutas donde el botón de inicio no debe mostrarse.
+    const noInicioRoutes = [
+      '/admin',
+      '/docente',
+      '/estudiante',
+      '/register',
+      '/lista',
+      '/materia',
+    ];
+
+    // Verificar si la URL actual está en la lista de rutas donde el botón de inicio no debe mostrarse.
+    return !noInicioRoutes.includes(currentUrl);
+  }
+
+  // Esta función verifica si la URL actual, está en la lista de rutas donde el botón de nosotros no debe mostrarse. Si es así, no muestra el botón de nosotros.
+  showNosotros(): boolean {
+    // Obtener la URL actual
+    const currentUrl = this.router.url;
+
+    // Lista de rutas donde el botón de nosotros no debe mostrarse.
+    const noNosotrosRoutes = [
+      '/admin',
+      '/docente',
+      '/estudiante',
+      '/register',
+      '/lista',
+      '/materia',
+    ];
+
+    // Verificar si la URL actual está en la lista de rutas donde el botón de nosotros no debe mostrarse.
+    return !noNosotrosRoutes.includes(currentUrl);
+  }
+
+  // Esta función verifica si la URL actual está en la lista de rutas donde el botón de inicio de sesión no debe mostrarse. Si es así, no muestra el botón de inicio de sesión.
   login(): boolean {
     // Obtener la URL actual
     const currentUrl = this.router.url;
-    if (currentUrl == '/admin') {
-      return currentUrl !== '/admin';
-    } else {
-      if (currentUrl == '/docente') {
-        return currentUrl !== '/docente';
-      } else {
-        if (currentUrl == '/estudiante') {
-          return currentUrl !== '/estudiante';
-        }
-      }
-    }
-    return currentUrl == '/inicio';
+
+    // Lista de rutas donde el botón de inicio de sesión no debe mostrarse.
+    const noLoginRoutes = [
+      '/admin',
+      '/docente',
+      '/estudiante',
+      '/register',
+      '/lista',
+      '/materia',
+    ];
+
+    // Verificar si la URL actual está en la lista de rutas donde el botón de inicio de sesión no debe mostrarse.
+    return !noLoginRoutes.includes(currentUrl);
   }
 
+  // Esta función verifica si la URL actual es '/admin' o '/inicio'. Si es así, no muestra el botón de perfil. Si la URL actual es '/docente', muestra el botón de perfil.
   perfil(): boolean {
     // Obtener la URL actual
     const currentUrl = this.router.url;
@@ -54,15 +97,18 @@ export class NavbarComponent {
     }
     return currentUrl == '/docente';
   }
+
+  // Esta función verifica si la URL actual es '/inicio' o '/nosotros'. Si es así, no muestra el botón de cierre de sesión. Si la URL actual es diferente muestra el botón de cierre de sesión.
   logout() {
     // Obtener la URL actual
     const currentUrl = this.router.url;
     if (currentUrl == '/inicio') {
       return currentUrl !== '/inicio';
     }
-    return currentUrl !== '/nosotros'
+    return currentUrl !== '/nosotros';
   }
 
+  // Esta función llama al método de cierre de sesión del servicio de autenticación.
   cerrarsesion() {
     this.afAuth.logout();
   }
