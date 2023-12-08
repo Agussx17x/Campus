81a5b92bfff510df8a1e2ee219caa3de73aad9e5
Aguss <agussx17x@gmail.com>
2023-12-08 00:23:05 -0300

    cambios en el table, materias independientes

--- src/app/modules/inicio/page/home/components/table/table.component.ts
+++ src/app/modules/inicio/page/home/components/table/table.component.ts
@@ -1,10 +1,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CrudService } from '../../services/crud.service';
 import { Seccion } from 'src/app/models/seccion';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Usuario } from 'src/app/models/usuario';
-import { AuthService } from 'src/app/modules/auth/services/auth.service';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Material } from 'src/app/models/material';
 
 @Component({
   selector: 'app-table',
@@ -13,112 +12,94 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class TableComponent {
   @ViewChild('fileInput') fileInput!: ElementRef;
-
-  // Variable para almacenar el archivo seleccionado por el usuario.
   private file!: File;
-
-  // Varaible para almacenar las secciones.
+  materialesPorSeccion: { [idSeccion: string]: Material[] } = {};
   secciones: Seccion[] = [];
-
-  // Varaible para almacenar los datos de las secciones.
   nuevaSeccion = { titulo: '', descripcion: '' };
-
-  // Varaible para almacenar los usuarios.
   usuario!: Usuario;
-
-  // Variable para almacenar los datos de los materiales.
-  nuevoMaterial = {
-    titulo: '',
-    descripcion: '',
-    fechaEntrega: '',
-    urlDescarga: '',
-  };
-
-  materialesPorSeccion: any = {};
+  idSeccionSeleccionada!: string;
+  idMateria!: string;
+  nuevoMaterial = { titulo: '', descripcion: '', fechaEntrega: '', url: '' };
 
   constructor(
     private crudService: CrudService,
-    private router: Router,
-    private authService: AuthService,
-    private firestore: AngularFirestore
+    private route: ActivatedRoute
   ) {}
 
-  async ngOnInit() {
-    // Obtiene el ID del usuario actual.
-    const uid = await this.authService.getuid();
-
-    // Si el usuario existe, obtiene sus datos de Firestore.
-    if (uid) {
-      const doc = await this.firestore
-        .collection('usuarios')
-        .doc(uid)
-        .get()
-        .toPromise();
-      if (doc && doc.exists) {
-        this.usuario = doc.data() as Usuario;
-      }
-    }
-
-    // Obtiene las secciones.
-    this.obtenerSecciones();
-  }
-
-  // Esta función obtiene las secciones.
-  obtenerSecciones() {
-    this.crudService.obtenerSecciones().subscribe((secciones) => {
-      this.secciones = secciones;
-      for (let seccion of secciones) {
-        this.obtenerMateriales(seccion.idSeccion);
-      }
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.idMateria = params['id']; // Aquí está el id de la materia
+      this.crudService.obtenerSecciones(this.idMateria).subscribe((actions) => {
+        this.secciones = actions.map((a) => {
+          const data = a.payload.doc.data() as Seccion;
+          const id = a.payload.doc.id;
+          // Obtiene los materiales de esta sección
+          this.crudService
+            .obtenerMateriales(this.idMateria, id)
+            .subscribe((actions) => {
+              this.materialesPorSeccion[id] = actions.map((a) => {
+                const data = a.payload.doc.data() as Material;
+                const id = a.payload.doc.id;
+                return { ...data, id };
+              });
+            });
+          return { ...data, id };
+        });
+      });
     });
   }
-
-  // Esta función crea una nueva sección.
+  // Crear seccion dentro de la coleccion de materias.
   crearSeccion() {
-    this.crudService.crearSeccion(this.nuevaSeccion).then(() => {
-      this.nuevaSeccion = { titulo: '', descripcion: '' };
-      this.obtenerSecciones();
-    });
+    if (this.nuevaSeccion.titulo && this.idMateria) {
+      this.crudService
+        .crearSeccion(this.idMateria, this.nuevaSeccion)
+        .then(() => {
+          console.log('Sección creada con éxito');
+          this.nuevaSeccion = { titulo: '', descripcion: '' }; // Restablece el formulario
+        })
+        .catch((error) => {
+          console.error('Error al crear la sección:', error);
+        });
+    } else{
+      console.log('Error al cargar sección.')
+    }
   }
-
-  // Esta función obtiene los materiales de una sección.
-  obtenerMateriales(idSeccion: any) {
-    this.crudService.obtenerMateriales(idSeccion).subscribe((materiales) => {
-      this.materialesPorSeccion[idSeccion] = materiales;
-    });
+  // Crear material dependiendo del id de la seccion, dentro de la coleccion de secciones.
+  crearMaterial(idSeccion: string) {
+    console.log('crear material');
+    if (this.nuevoMaterial.titulo && this.idMateria && idSeccion) {
+      console.log('dentro del if');
+      // Primero, sube el archivo al almacenamiento de Firebase y obtén la URL de descarga.
+      this.crudService.uploadFile(this.file).then((url) => {
+        console.log('cargando archivo');
+        // Agrega la URL de descarga al objeto nuevoMaterial.
+        this.nuevoMaterial.url = url;
+        // Luego, crea el documento en Firestore.
+        this.crudService
+          .crearMaterial(this.idMateria, idSeccion, this.nuevoMaterial)
+          .then(() => {
+            console.log('Material creado con éxito');
+            this.nuevoMaterial = {
+              titulo: '',
+              descripcion: '',
+              fechaEntrega: '',
+              url: '',
+            };
+            // Restablece el formulario
+            console.log('restablece el formulario');
+          })
+          .catch((error) => {
+            console.error('Error al crear el material:', error);
+          });
+      });
+    } else {
+      console.log('uno o varios no existen');
+    }
   }
-
-  // Esta función menja la selección de archvo.
+  // Esta función maneja la selección de archivo.
   onFileSelected(event: any) {
     this.file = event.target.files[0];
   }
-
-  // Esta función crea un nuevo material.
-  async crearMaterial(idSeccion: any) {
-    if (this.file) {
-      const url = await this.crudService.uploadFile(this.file);
-      this.nuevoMaterial.urlDescarga = url;
-    }
-    this.crudService.crearMaterial(idSeccion, this.nuevoMaterial).then(() => {
-      this.nuevoMaterial = {
-        titulo: '',
-        descripcion: '',
-        fechaEntrega: '',
-        urlDescarga: '',
-      };
-      this.obtenerMateriales(idSeccion);
-    });
-  }
-
-  // Esta función deternminasu se deben mostrar los botones.
-  botones() {
-    // Obtiene la URL actual.
-    const currentUrl = this.router.url;
-    return currentUrl !== '/estudiante/materia';
-  }
-
-  mostrarEditar() {}
-
   // Método para obtener/guardar el estado de la sección y cambiar su estado.
   toggleSectionState(seccion: Seccion): void {
     // Obtén el estado actual de la sección desde LocalStorage.
@@ -132,8 +113,20 @@ export class TableComponent {
       JSON.stringify(newState)
     );
   }
-
-  // Método para determinar si la sección está expandida o no
+  // Obtener las secciones en firestore database.
+  obtenerSecciones() {
+    this.route.params.subscribe((params) => {
+      const materiaId = params['id']; // Aquí está el id de la materia
+      this.crudService.obtenerSecciones(materiaId).subscribe((actions) => {
+        this.secciones = actions.map((a) => {
+          const data = a.payload.doc.data() as Seccion;
+          const id = a.payload.doc.id;
+          return { ...data, id };
+        });
+      });
+    });
+  }
+  // Método para determinar si la sección está expandida o no.
   isSectionExpanded(seccion: Seccion): boolean {
     // Obtiene el estado actual de la sección desde LocalStorage.
     const currentState = localStorage.getItem(
