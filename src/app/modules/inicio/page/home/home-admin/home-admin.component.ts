4bbb270c154a246c5d098272347579236bcb4844
Aguss <agussx17x@gmail.com>
2023-12-12 18:38:44 -0300

    Co-authored-by: Bravo Franco <FrankzeB@users.noreply.github.com>

--- src/app/modules/inicio/page/home/home-admin/home-admin.component.ts
+++ src/app/modules/inicio/page/home/home-admin/home-admin.component.ts
@@ -5,8 +5,8 @@ import { CrudService } from 'src/app/modules/inicio/page/home/services/crud.serv
 import { getStorage, ref, uploadBytes } from 'firebase/storage';
 import { Router } from '@angular/router';
 import { Avisos } from 'src/app/models/avisos';
-import { Materia } from 'src/app/models/materia';
 import { AvisosService } from '../services/avisos.service';
+import { Materia } from 'src/app/models/materia';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ListaUsuariosService } from '../services/lista-usuarios.service';
 
@@ -20,6 +20,7 @@ export class HomeAdminComponent {
   coleccionTrabajos: Trabajos[] = [];
   coleccionAvisos: Avisos[] = [];
   coleccionMateria: Materia[] = [];
+
   // FormGroups
   trabajo = new FormGroup({
     titulo: new FormControl('', Validators.required),
@@ -36,8 +37,11 @@ export class HomeAdminComponent {
     titulo: new FormControl('', Validators.required),
     icono: new FormControl('', Validators.required),
   });
-  
-  
+
+  // Declaro las variables para almacenar el aviso seleccionado y el usuario.
+  avisoSeleccionado!: Avisos;
+  imagenSeleccionada!: File;
+
   nombre: string = '';
   apellido: string = '';
   tipoCredencial: string = '';
@@ -47,6 +51,7 @@ export class HomeAdminComponent {
     doc: 'Docente',
     est: 'Estudiante',
   };
+
   constructor(
     private crudService: CrudService,
     private avisosService: AvisosService,
@@ -56,46 +61,103 @@ export class HomeAdminComponent {
   ) {}
   ngOnInit(): void {
     // Luego, obtén la lista de avisos al inicializar el componente
-    this.avisosService.obtenerAvisos().subscribe((avisos)=>{
+    this.avisosService.obtenerAvisos().subscribe((avisos) => {
       this.coleccionAvisos = avisos;
     });
+
+    // Nos suscribimos al BehaviorSubject avisoSeleccionado$ del servicio avisos.
+    this.avisosService.avisoSeleccionado$.subscribe((aviso) => {
+      // Verifica si el aviso emitido no es null.
+      if (aviso) {
+        // Actualizamos la propiedad avisoSeleccionado del componente con el aviso emitido.
+        this.avisoSeleccionado = aviso;
+        // Actualizamos los valores del formulario.
+        this.avisos.setValue({
+          titulo: aviso.titulo,
+          descripcion: aviso.descripcion,
+        });
+      }
+    });
+
     this.materias.setValue({
       idMateria: this.crudService.crearId(),
       titulo: '',
       icono: '',
     });
-    // Mostrar usuario.
-    this.afAuth.authState.subscribe((user)=>{
+
+    // Muestra el nombre, apellido y credencial del usuario actual suscribiendose.
+    this.afAuth.authState.subscribe((user) => {
       if (user) {
+        // Si el usuario se logea, obtiene el uid del usuario atenticado.
         const uidUsuarioLogueado = user.uid;
 
-        this.listaUsuariosService.obtenerUsuarios().subscribe((usuarios)=>{
+        // Luego, obtiene la lista de usuarios.
+        this.listaUsuariosService.obtenerUsuarios().subscribe((usuarios) => {
+          // Encuentra al usuario autenticado en la lista de usuarios.
           const usuarioLogueado = usuarios.find(
             (u) => u.uid === uidUsuarioLogueado
           );
+
+          // Si el usuario autenticado se encuentra en la lista de usuarios
           if (usuarioLogueado) {
+            // Almacena el nombre y apellido del usuario.
             this.nombre = usuarioLogueado.nombre;
             this.apellido = usuarioLogueado.apellido;
             // Traduce la credencial abreviada.
             this.tipoCredencial =
-              this.credencialesMapping[usuarioLogueado.credencial]||
+              this.credencialesMapping[usuarioLogueado.credencial] ||
               'Usuario sin tipo';
           }
         });
       }
     });
   }
+
   selectDay!: string;
-  async agregarAviso(){
+
+  // Esta función lo que hace es agregar un nuevo aviso.
+  async agregarAviso() {
+    // Verifica si los campos están todos completos.
+    if (!this.avisos.value.titulo || !this.avisos.value.descripcion) {
+      alert('Por favor, complete todos los campos.');
+      return;
+    }
+
+    // Verifica si los avisos han alcanzado un máximo de 6.
+    if (this.coleccionAvisos.length >= 6) {
+      alert(
+        'Ya has alcanzado el máximo de avisos por hoy, por favor elimine los ya existentes o espere a seguir en otro plazo.'
+      );
+      return;
+    }
+
+    // Si el formulario es valido crea un nuevo aviso.
     if (this.avisos.valid) {
+      const idAviso = this.avisosService.generarId();
+      console.log(`ID del aviso generado: ${idAviso}`);
+
+      // Genera el ID del aviso previo, para que se pueda guardar correctamente las imágenes.
       let nuevoAviso: Avisos = {
-        idAvisos: '',
+        idAvisos: idAviso,
         titulo: this.avisos.value.titulo!,
         descripcion: this.avisos.value.descripcion!,
+        imagenUrl: '',
       };
-      //ENVIAMOS ESE PRODUCTO
+
+      // Sube la imagen antes de crear el aviso.
+      if (this.imagenSeleccionada) {
+        alert('Subiendo imágen, espere por favor...');
+        nuevoAviso.imagenUrl = await this.avisosService.subirImagen(
+          this.imagenSeleccionada,
+          idAviso
+        );
+      }
+
+      console.log(nuevoAviso); // Registra el objeto nuevoAviso.
+
+      // Creas el aviso usando el mismo ID
       await this.avisosService
-        .crearAvisos(nuevoAviso)
+        .crearAvisos(idAviso, nuevoAviso)
         .then((avisos) => {
           alert('Ha agregado un nuevo aviso con éxito');
         })
@@ -104,7 +166,70 @@ export class HomeAdminComponent {
         });
     }
   }
-  crearMateria(){
+
+  // Esta función ajusta la altura al escribir en el textarea.
+  ajustarAltura(event: any) {
+    // Restablece la altura del textarea a 'auto'.
+    event.target.style.height = 'auto';
+    // Ajusta la altura del textarea a la altura de su contenido.
+    event.target.style.height = event.target.scrollHeight + 'px';
+  }
+
+  // Función para editar el aviso nuevo.
+  async editarAviso() {
+    if (this.avisoSeleccionado) {
+      // Preparamos los datos del aviso seleccionado.
+      let datos: Avisos = {
+        idAvisos: this.avisoSeleccionado.idAvisos,
+        titulo: this.avisos.value.titulo!,
+        descripcion: this.avisos.value.descripcion!,
+        imagenUrl: this.avisoSeleccionado.imagenUrl,
+      };
+
+      // Si se seleccionó una nueva imagen, sube la imagen y actualiza la URL de la imagen en los datos del aviso.
+      if (this.imagenSeleccionada) {
+        alert('Actualizando su imágen, espere por favor...');
+        datos.imagenUrl = await this.avisosService.subirImagen(
+          this.imagenSeleccionada,
+          this.avisoSeleccionado.idAvisos
+        );
+      }
+      // Llama al método 'modificarAvisos' del servicio 'avisosService' para actualizar el aviso en la base de datos.
+      this.avisosService
+        .modificarAvisos(this.avisoSeleccionado.idAvisos, datos)
+        .then((respuesta) => {
+          alert('El aviso ha sido editado con éxito.');
+        })
+        .catch((error) => {
+          alert(
+            'Ha habido un error al intentar editar el aviso, razón: \n' + error
+          );
+        });
+    } else {
+      alert('Por favor, selecciona un aviso antes de intentar editarlo.');
+    }
+  }
+
+  // Esta función maneja la sección de una imágen.
+  manejarSeleccionImagen(event: Event) {
+    // Obtiene el elemento HTML que disparo el input de tipo "File".
+    const input = event.target as HTMLInputElement;
+    // Verifica si el input tiene archivos y si el primer archivo existe.
+    if (input.files && input.files[0]) {
+      // Obtiene el primer archivo del input.
+      const file: File = input.files[0];
+      // Verifica si el archivo es una imagen.
+      if (!file.type.startsWith('image/')) {
+        // Si el archivo no es una imagen, muestra una alerta al usuario y termina la función.
+        alert('Por favor, selecciona una imagen.');
+        return;
+      }
+      // Si el archivo es una imagen, lo guarda en la propiedad "imagenSeleccionada".
+      this.imagenSeleccionada = file;
+    }
+  }
+
+  crearMateria() {
     if (this.materias.valid) {
       let data: Materia = {
         idMateria: this.materias.value.idMateria || this.crudService.crearId(),
--- src/app/modules/inicio/page/home/services/avisos.service.ts
+++ src/app/modules/inicio/page/home/services/avisos.service.ts
@@ -3,28 +3,38 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { aV } from '@fullcalendar/core/internal-common';
 import { map } from 'rxjs/operators';
 import { Avisos } from 'src/app/models/avisos';
+import { BehaviorSubject } from 'rxjs'; // BehaviorSubject es como un observable que puede emitir varios eventos, cargando siempre el último.
 
 @Injectable({
   providedIn: 'root',
 })
 export class AvisosService {
+  // Declara la colección avisos.
   avisosCollection: AngularFirestoreCollection<Avisos>;
 
-  constructor(private database: AngularFirestore) {
+  // Lo que hace es almacenar el aviso seleccionado para ser usado como "notificación". El signo $ es para decir que es un Observable.
+  public avisoSeleccionado$ = new BehaviorSubject<Avisos | null>(null);
+
+  constructor(
+    private database: AngularFirestore,
+    private storage: AngularFireStorage
+  ) {
+    // Inicializa la colección avisos.
     this.avisosCollection = database.collection('avisos');
   }
 
-  crearAvisos(avisos: Avisos) {
+  // Este método crea un nuevo aviso.
+  crearAvisos(id: string, avisos: Avisos) {
     return new Promise(async (resolve, reject) => {
       try {
-        // Toma el ID referenciando a la base de datos y crea un nuevo iD.
-        const id = this.database.createId();
+        // Asigna el ID proporcionado al aviso.
         avisos.idAvisos = id;
 
-        // Guarda ese nuevo ID.
+        // Guarda el aviso en Firestore usando el ID proporcionado.
         const resultado = await this.avisosCollection.doc(id).set(avisos);
         resolve(resultado);
       } catch (error) {
@@ -33,9 +43,54 @@ export class AvisosService {
     });
   }
 
+  // Este método obtiene los avisos.
   obtenerAvisos() {
     return this.avisosCollection
       .snapshotChanges()
       .pipe(map((action) => action.map((a) => a.payload.doc.data())));
+  }
+
+  // Este método modifica un nuevo aviso.
+  modificarAvisos(idAvisos: string, nuevaData: Avisos) {
+    return this.database.collection('avisos').doc(idAvisos).update(nuevaData);
+  }
+
+  // Este método elimina un aviso.
+  eliminarAvisos(idAvisos: string, aviso: Avisos) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        // Verifica si el aviso tiene una imagen.
+        if (aviso.imagenUrl) {
+          // Si el aviso tiene una imagen, elimínala.
+          const filePath = `avisos/${idAvisos}`;
+          await this.storage.ref(filePath).delete().toPromise();
+        }
+
+        // Elimina el aviso de Firestore Database.
+        const resp = await this.avisosCollection.doc(idAvisos).delete();
+        resolve(resp);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  // Método para generar un nuevo ID.
+  generarId(): string {
+    // Utiliza el metodo "createId" para generar un nuevo ID.
+    return this.database.createId();
+  }
+
+  // Método para subir una imágen a FireStorage.
+  subirImagen(imagen: File, idAviso: string): Promise<string> {
+    // Define la ruta del archivo en Firebase Storage.
+    const filePath = `avisos/${idAviso}`;
+    // Obtiene una referencia al archivo en Firebase Storage.
+    const ref = this.storage.ref(filePath);
+    console.log(`Subiendo imagen con ID: ${idAviso}`);
+    // Sube la imagen a Firebase Storage y luego obtiene la URL de descarga de la imagen.
+    return this.storage.upload(filePath, imagen).then(() => {
+      return ref.getDownloadURL().toPromise();
+    });
   }
 }
